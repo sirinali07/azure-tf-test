f7965304e72e95d1357a94d1d7de992184476b86
Sirin Ali <118505493+sirinali07@users.noreply.github.com>
2024-12-02 15:26:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@
   resource_provider_registrations = "none"
 
   # Connection to Azure
-  subscription_id = var.subscription_id
+  subscription_id = 607e05a3-6e68-4027-bef6-4184bd3922b9
   client_id = var.client_id
   client_secret = var.client_secret
   tenant_id = var.tenant_id
